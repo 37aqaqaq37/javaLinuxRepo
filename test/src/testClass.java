3639a4fa29586fc05369d4d50df31cad34f7905c
Ammad <37aqaqaq37@gmail.com>
2019-11-23 00:38:20 +0000

    Test java file in Linux Mint

--- test/src/testClass.java
+++ test/src/testClass.java
@@ -1,2 +1,15 @@
 public class testClass {
+
+    public static void main(String[] args){
+
+        System.out.println("Hello World");
+        String phrase = "This is a piece of test java code";
+        System.out.println(phrase);
+        int x = 30;
+        for (int i = 0; i<5; ++i){
+            System.out.println(i+1);
+
+        }
+
+    }
 }
